c731e8f961aa1a3e6c5a6118a8655cda5f6e0901
Avinash Guleria <avinashguleria1009@gmail.com>
2025-06-26 22:13:02 +0530

    dashboard half completed pie chart and shows expenses

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -4,6 +4,13 @@ import { useUserAuth } from "../../hooks/useUserAuth";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/ApiPaths";
+import InfoCard from "../../components/cards/InfoCard";
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+import { IoMdCard } from "react-icons/io";
+import { addThousandsSeparator } from "../../utils/helper";
+import RecentTransactions from "../../components/Dashboard/RecentTransactions";
+import FinanceOverview from "../../components/Dashboard/FinanceOverview";
+import ExpenseTransactions from "../../components/Dashboard/ExpenseTransactions";
 
 const Home = () => {
   useUserAuth();
@@ -34,14 +41,52 @@ const Home = () => {
 
   useEffect(() => {
     fetchDashboardData();
-    return () => {
-      
-    };
+    return () => {};
   }, []);
 
   return (
     <DashboardLoayout activeMenu="Dashboard">
-      <div className="my-5 mx-auto"></div>
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance"
+            value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+            color="bg-primary"
+          />
+
+          <InfoCard
+            icon={<LuWalletMinimal />}
+            label="Total Income"
+            value={addThousandsSeparator(dashboardData?.totalIncome || 0)}
+            color="bg-orange-500"
+          />
+
+          <InfoCard
+            icon={<LuHandCoins />}
+            label="Total Expense"
+            value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
+            color="bg-red-500"
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+          {/* <RecentTransactions
+            transactions={dashboardData?.recentTransactions}
+            onSeeMore={() => navigate("/expense")}
+          />
+
+          <FinanceOverview
+            totalBalance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpense={dashboardData?.totalExpense || 0}
+          /> */}
+
+          <ExpenseTransactions
+            transactions={dashboardData?.last30DaysExpenses?.transactions || []}
+            onSeeMore={() => navigate("/expense")}
+          />
+        </div>
+      </div>
     </DashboardLoayout>
   );
 };
--- frontend/expense-tracker/src/utils/helper.js
+++ frontend/expense-tracker/src/utils/helper.js
@@ -9,8 +9,19 @@ export const getInitials = (name) => {
   const words = name.split(" ");
   let initials = "";
 
-  for(let i = 0; i < Math.min(words.length, 2); i++){
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
     initials += words[i][0];
   }
   return initials.toUpperCase();
-}
+};
+
+export const addThousandsSeparator = (num) => {
+  if (num == null || isNaN(num)) return "";
+
+  const [integerPart, fractionalPart] = num.toString().split(".");
+  const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  return fractionalPart
+    ? `${formattedInteger}.${fractionalPart}`
+    : formattedInteger;
+};
